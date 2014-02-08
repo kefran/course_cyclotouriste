379392d2c2a93cd9ce46210c377fb83d1ec6cef7
kefran <franckschn@gmail.com>
2014-02-08 13:31:17 +0100

    début de création des pages en php

--- appli/fr/liste_courses.php
+++ appli/fr/liste_courses.php
@@ -1,4 +1,51 @@
-<?php
-//print_r(C:\appli_lionne_dev\course_cyclotouriste\appli\$_SERVER);
-require_once('C:\appli_lionne_dev\course_cyclotouriste\appli\common\init.php');
-?>
+<html>
+    <head>
+        <meta http-equiv="X-UA-Compatible" content="IE=8">
+        <title>Site des gestion de la course de la LIONNE</title>
+        <link href="../style.css" rel="stylesheet" type="text/css">
+        <link href="../bootstrap/css/bootstrap.min.ie.css" rel="stylesheet" media="screen">
+        <script src="http://code.jquery.com/jquery.js"></script>
+        <script src="../bootstrap/js/bootstrap.min.js"></script>
+        <script src="../common/xhr.js" ></script>
+        <link rel="StyleSheet" type="text/css" href="../common/page.css" />
+    </head>
+    <body>
+        <?php include_once('../common/menu.html'); ?>
+        <div id="wrapper">
+            <h2> Listes des courses </h2>
+            <table id="liste_course" class="table table-bordered">
+                <tr>
+                    <th>Numéro</th>
+                    <th>Date</th>
+                    <th>Année</th>
+                    <th>Total</th>
+                    <th>Circuit 1</th>
+                    <th>Circuit 2</th>
+                    <th>Circuit 3</th>
+                    <th></th>
+                </tr>
+                <tr>
+                    <td>25</td><td>02/09/2012</td><td>2012</td><td>421</td><td>160</td><td>184</td><td>77</td><td><i class="icon icon-edit"></i>
+                </tr>
+                <tr>
+                    <td>25</td><td>02/09/2012</td><td>2012</td><td>421</td><td>160</td><td>184</td><td>77</td><td><i class="icon icon-edit"></i>
+                </tr>
+                <tr>
+                    <td>25</td><td>02/09/2012</td><td>2012</td><td>421</td><td>160</td><td>184</td><td>77</td><td><i class="icon icon-edit"></i>
+                </tr>
+                <tr>
+                    <td>25</td><td>02/09/2012</td><td>2012</td><td>421</td><td>160</td><td>184</td><td>77</td><td><i class="icon icon-edit"></i>
+                </tr>
+                <tr>
+                    <td>25</td><td>02/09/2012</td><td>2012</td><td>421</td><td>160</td><td>184</td><td>77</td><td><i class="icon icon-edit"></i>
+                </tr>
+                <tr>
+                    <td>25</td><td>02/09/2012</td><td>2012</td><td>421</td><td>160</td><td>184</td><td>77</td><td><i class="icon icon-edit"></i>
+                </tr>
+                <tr>
+                    <td>25</td><td>02/09/2012</td><td>2012</td><td>421</td><td>160</td><td>184</td><td>77</td><td><i class="icon icon-edit"></i>
+                </tr>
+            </table>
+        </div>  
+    </body>
+</html>
